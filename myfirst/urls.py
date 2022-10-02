f84e25f51968244ef577d872e8e2c8643b6720e2
glebfedorov911 <114831334+glebfedorov911@users.noreply.github.com>
2022-10-02 14:52:20 +0300

    Update urls.py

--- myfirst/urls.py
+++ myfirst/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('articles/', include('articles.urls')),
     path('admin/', admin.site.urls),
-    path('grappelli/', include('grappelli.urls')),
 ]
